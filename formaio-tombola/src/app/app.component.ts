30a350bccae35fe4688b75c777ebd928318062d8
filippo formaio <filippo.formaio@gmail.com>
2020-04-20 20:31:35 +0000

    fine programma

--- formaio-tombola/src/app/app.component.ts
+++ formaio-tombola/src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formaio-tombola';
+  numeriUsciti: Array<number>=[];
+  ultimoNumero: string="...";
+  generaNumero(){
+    var check=false;
+    //ciclo per non estrarre duplicati
+    //il ciclo si ripete finché non si trova un numero diverso
+    while(check==false){
+      var numero= Math.round(Math.random()*99+1);
+      check=true;
+      for (var i=0; i< this.numeriUsciti.length; i++){
+        if (numero==this.numeriUsciti[i]){
+          check=false;
+        }
+      }
+      if (check){
+        this.numeriUsciti.push(numero);
+        console.log(numero); //limiti inclusi
+        var riga=document.getElementById("visuaNumero");
+        riga.innerHTML=""+numero;
+        this.ultimoNumero=""+numero;
+        if (this.numeriUsciti.length==100){
+          //i numeri sono finiti
+          console.log("numeri finit!");
+          var bottone= document.getElementById("bottone");
+          bottone.setAttribute("disabled","disabled");
+          alert("Tutti i numeri sono stati estratti!");
+          return false;
+        }
+      }
+    }
+    return false;
+  }
 }
---
+++ formaio-tombola/src/app/cartella/cartella.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cartella',
+  templateUrl: './cartella.component.html',
+  styleUrls: ['./cartella.component.css']
+})
+export class CartellaComponent implements OnInit {
+  @Input() numeriEstratti: string[];
+  @Input() ultimoNumero: string;
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
